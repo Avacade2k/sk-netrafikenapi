01d32fa45ac50b3f4a8056247eb25efba9f7ccab
Dawid Dymarkowski <avacade2k@gmail.com>
2020-01-27 09:02:15 +0100

    index servlet and css added

---
+++ WebContent/index.jsp
@@ -0,0 +1,21 @@
+  
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Skånetrafiken API</title>
+</head>
+<body>
+
+   <form action="SkaneApi" method="get">  
+    City:<input type="text" name="city"/><br/>  
+    Bus stop:<input type="text" name="busstop"/><br/>  
+    
+    <input type="submit" value="go"/>  
+    </form>  
+
+
+</body>
+</html>
---
+++ src/api/SkaneApi.java
@@ -0,0 +1,71 @@
+package api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+
+/**
+ * Servlet implementation class SkaneApi
+ */
+@WebServlet("/SkaneApi")
+public class SkaneApi extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SkaneApi() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String cityStr = request.getParameter("city");
+		out.print("<br>");
+		out.print("City= " + cityStr);
+
+		out.print("<br>");
+
+		String busStr = request.getParameter("busstop");
+		out.print("Bus stop= " + busStr);
+		out.print("<br>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
